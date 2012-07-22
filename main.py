1504642cc4407d0085f6a89d3ddd514211c16a52
Chris Bunch <shatterednirvana@gmail.com>
2012-07-22 13:06:55 -0700

    having model page query models from datastore, and added documentation on model class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import cgi
 import datetime
+import logging
 import re
 import wsgiref.handlers
 
@@ -11,6 +12,16 @@ from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+
+
+class StochKitModelWrapper(db.Model):
+  """
+    A StochKitModelWrapper is a representation of the biochemical model that 
+      runs with the open source StochKit2.0 software package. It contains the 
+      actual model itself and will eventually be expanded to include metadata 
+      about the model.
+  """
+  model = db.TextProperty()
 
 
 class MainPage(webapp.RequestHandler):
@@ -27,11 +38,21 @@ class UploadModelPage(webapp.RequestHandler):
 
 class RunPage(webapp.RequestHandler):
   def get(self):
+    model_query = db.GqlQuery("SELECT * FROM StochKitModelWrapper")
+    models = [model for model in model_query]
+    if len(models) > 0:
+      models_present = True
+    else:
+      models_present = False
+
     self.response.out.write(template.render('templates/run.html', 
-                                            {'active_run': True}))
+                                            {'active_run': True,
+                                             'models': models,
+                                             'models_present': models_present}))
 
 
 def main():
+  logging.getLogger().setLevel(logging.DEBUG)
   wsgiref.handlers.CGIHandler().run(webapp.WSGIApplication([
     ('/', MainPage),
     ('/upload', UploadModelPage),
